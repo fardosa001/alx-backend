1b0b9dd77463d827171c834fd0add661ae66289a
fardosa001 <fardosaadow@gmail.com>
2024-04-01 17:03:03 +0000

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+
+
+def index_range(page, page_size):
+    """ takes two integer arguments page and page_size and
+    return a tuple of size two containing a
+    start index and an end index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
