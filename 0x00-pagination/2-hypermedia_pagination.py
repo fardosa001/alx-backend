7ea6c9913074f0148aaf79f2259096fac27a485b
fardosa001 <fardosaadow@gmail.com>
2024-04-01 17:36:29 +0000

    Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""Hipermedia pagination"""
+import csv
+import math
+from typing import List, Dict, Any
+
+
+def index_range(page, page_size):
+    """ takes two integer arguments page and page_size and
+    return a tuple of size two containing a
+    start index and an end index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page of data from the dataset."""
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+        total_rows = len(dataset)
+        pages = []
+        if start >= total_rows:
+            return pages
+        pages = self.dataset()
+        return pages[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """Get hyper data for a given page."""
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+
+        dataset_page = self.get_page(page, page_size)
+        dataset_length = len(dataset_page)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        next_page = page + 1 if dataset_length == page_size else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": dataset_length,
+            "page": page,
+            "data": dataset_page,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
